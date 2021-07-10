e2304d99c4012787a9f5470d5d8c39e6b204ee17
Franco <franconunez500@gmail.com>
2021-07-10 18:21:37 -0300

    funcionalidad principal  hecha, realizar trape de errores y pulir styles

--- src/components/formulario.js
+++ src/components/formulario.js
@@ -2,12 +2,14 @@
 
 
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 // componentes boostrap
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Col from 'react-bootstrap/Col'
+
+import { db } from '../config/firebase'
 
 
 export default function Formulario(props) {
@@ -19,7 +21,6 @@ export default function Formulario(props) {
     const valoresIniciales = {
         titulo: '',
         descripcion: '',
-        urgencia: ''
     }
 
     const [valores, setValores] = useState(valoresIniciales);
@@ -31,9 +32,26 @@ export default function Formulario(props) {
     }
 
     const handleChange = e => {
+        console.log(e.target)
         const { name, value } = e.target;
         setValores({ ...valores, [name]: value })
+        console.log(valores)
     }
+
+
+    const obtenerMedianteId = async (id) =>{
+        const doc = await db.collection('Tareas').doc(id).get();
+        setValores({...doc.data()})
+    }
+
+    useEffect(()=>{
+        console.log(props.idActual);
+        if (props.idActual === ""){
+            setValores({...valoresIniciales});
+        }else{
+            obtenerMedianteId(props.idActual); 
+        }
+    }, [props.idActual]);
 
 
 
@@ -41,24 +59,29 @@ export default function Formulario(props) {
 
         <Col className="col-12 col-sm-5 col-md-4">
             <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="formBasicTitle">
+                <Form.Group>
                     <Form.Label>Titulo</Form.Label>
-                    <Form.Control type="text" name="Titulo" value={valores.titulo} onChange={handleChange} />
+                    <Form.Control 
+                    type="text" 
+                    name="titulo" 
+                    value={valores.titulo} 
+                    onChange={handleChange} />
                 </Form.Group>
-                <Form.Group controlId="formBasicDescription">
+                <Form.Group>
                     <Form.Label>Descripción</Form.Label>
-                    <Form.Control type="text" name="Descripcion" value={valores.descripcion} onChange={handleChange} />
+                    <Form.Control 
+                    type="text" 
+                    name="descripcion" 
+                    value={valores.descripcion} 
+                    onChange={handleChange} />
                 </Form.Group>
-                <Form.Group controlId="exampleForm.SelectCustom">
-                    <Form.Label>Nivel de urgencia: </Form.Label>
-                    <Form.Control as="select" name="Urgencia" value={valores.urgencia} onChange={handleChange} custom>
-                        <option>Poco</option>
-                        <option>Normal</option>
-                        <option>Mucho</option>
-                    </Form.Control>
-                </Form.Group>
-                <Button variant="primary" className="mt-4" size="lg" block>
-                    Agregar
+                <Button 
+                type="submit"
+                variant="primary" 
+                className="mt-4" 
+                size="lg" 
+                block>
+                    {props.idActual === '' ? 'Guardar' : 'Actualizar'}
                 </Button>
             </Form>
         </Col>
--- src/components/tablero.js
+++ src/components/tablero.js
@@ -20,12 +20,30 @@ import Button from 'react-bootstrap/esm/Button';
 export default function Tablero() {
 
     const [tareas, setTareas] = useState([])
+    const [idActual, setidActual] = useState('')
 
-    const agregarTarea =  async (valores) =>{    
-        await db.collection('Tareas').doc().set(valores);
+    const agregarTarea =  async (valores) =>{
+        try {
+            if(idActual == ''){
+                await db.collection('Tareas').doc().set(valores);
+            }
+            else{
+                await db.collection('Tareas').doc(idActual).update(valores);
+            }
+        } catch (error) {
+            console.error(error)
+        }
     }
 
+    const eliminarTarea = async (id) =>{
+        if (window.confirm("Quieres eliminar esta tarea?")){
+            await db.collection("Tareas").doc(id).delete();
+        }
+    };
 
+
+
+    
     const obtenerTareas = async () => {
         db.collection('Tareas').onSnapshot((querySnapshot) =>{
             const docs = [];
@@ -43,18 +61,27 @@ export default function Tablero() {
 
         return (
             <>
-                <Formulario {...{agregarTarea, obtenerTareas}}/>
-                <Col className="col-12 col-sm-6 col-md-8 d-flex flex-wrap justify-content-center mt-4">
+                <Formulario {...{agregarTarea, obtenerTareas, idActual}}/>
+                <Col className="col-12 col-sm-6 col-md-8 d-flex flex-wrap justify-content-center mt-4" key={tareas.id}>
+                    <h2>Tareas</h2>
                     {
                         tareas.map(tarea => (
                             <Card className="col-12 mt-3">
                             <Card.Body>
-                                <Card.Title>{tarea.titulo} {tarea.urgencia}</Card.Title>
+                                <Card.Title>{tarea.titulo}</Card.Title>
                                 <Card.Text>
                                     {tarea.descripcion}
                                 </Card.Text>
-                                <Button size="sm" className="m-2" variant="warning"><i class="far fa-edit"></i></Button>
-                                <Button size="sm" variant="danger"><i class="far fa-trash-alt"></i></Button>
+                                <Button size="sm" className="m-2" variant="warning">
+                                    <i class="far fa-edit" 
+                                    onClick={()=>setidActual(tarea.id)}>
+                                    </i>
+                                    </Button>
+                                <Button size="sm" variant="danger">
+                                    <i class="far fa-trash-alt"
+                                    onClick={()=>eliminarTarea(tarea.id)}>
+                                    </i>
+                                    </Button>
                             </Card.Body>
                         </Card>
                         ))}            
