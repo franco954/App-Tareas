3e30c3cae9831b718a86cdc6737a5c4069fc50d8
Franco <franconunez500@gmail.com>
2021-08-05 19:11:47 -0300

    add react hook form + yup

--- src/components/formulario.js
+++ src/components/formulario.js
@@ -6,18 +6,46 @@ import { useState, useEffect } from 'react';
 
 // componentes boostrap
 import Form from 'react-bootstrap/Form'
+import { FormText } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button'
 import Col from 'react-bootstrap/Col'
 
+// firebase
 import { db } from '../config/firebase'
+
+// react hook + yup
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+
 
 
 export default function Formulario(props) {
 
+    const validationSchema = Yup.object().shape({
+        titulo: Yup.string().required('El titulo de la tarea es obligatorio'),
+        descripcion: Yup.string().required('La descripcion de la tarea es obligatoria')
+      });
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors }
+      } = useForm({
+        resolver: yupResolver(validationSchema)
+    });
+
+    const onSubmit = data => {
+        console.log(JSON.stringify(data, null, 2));
+        // e.preventDefault()
+        props.agregarTarea(valores)
+        setValores({...valoresIniciales})
+    };
+
+
+    
     console.log(props.setidActual)
-
-
-
     const valoresIniciales = {
         titulo: '',
         descripcion: '',
@@ -26,14 +54,10 @@ export default function Formulario(props) {
     const [valores, setValores] = useState(valoresIniciales);
 
     const handleCancel = () =>{
+        reset()
         props.setidActual('')
     }
 
-    const handleSubmit = e => {
-        e.preventDefault()
-        props.agregarTarea(valores)
-        setValores({...valoresIniciales})
-    }
 
     const handleChange = e => {
         console.log(e.target)
@@ -62,28 +86,32 @@ export default function Formulario(props) {
     return (
 
         <Col className="col-12 col-md-4 mt-md-4">
-            <Form onSubmit={handleSubmit}>
+            <Form onSubmit={handleSubmit(onSubmit)}>
                 <Form.Group className="mt-2">
                     <Form.Label className="fw-bolder text-muted">Titulo:</Form.Label>
                     <Form.Control 
-                    className="bg-dark text-white"
+                    className={`form-control bg-dark text-white ${errors.titulo ? 'is-invalid' : ''}`}
+                    {...register('titulo')}
                     autoComplete="off"
-                    required
+                    // required
                     type="text" 
                     name="titulo" 
                     value={valores.titulo} 
                     onChange={handleChange} />
+                    <FormText className="invalid-feedback">{errors.titulo ?.message}</FormText>
                 </Form.Group>
                 <Form.Group className="mt-2">
                     <Form.Label className="fw-bolder text-muted">Descripción:</Form.Label>
                     <Form.Control 
-                    className="bg-dark text-white"
+                    className={`form-control bg-dark text-white ${errors.descripcion ? 'is-invalid' : ''}`}
+                    {...register('descripcion')}
                     autoComplete="off"
-                    required
+                    // required
                     type="text" 
                     name="descripcion" 
                     value={valores.descripcion} 
                     onChange={handleChange} />
+                    <FormText className="invalid-feedback">{errors.descripcion ?.message}</FormText>
                 </Form.Group>
                 <Form.Group className="d-flex justify-content-center">
                     <Button 
